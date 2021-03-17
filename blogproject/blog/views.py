2d6c081153cbebf1c73879263ed940dc3b6b57a7
makule <goodymakule@gmail.com>
2021-03-17 15:15:10 +0300

    day 2 of proj

--- blogproject/blog/views.py
+++ blogproject/blog/views.py
@@ -1,3 +1,27 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+posts = [
+    {
+        'author'     : 'makule',
+        'title'      : 'blog post 1',
+        'content'    : 'first post content',
+        'date_posted': 'march 17 2021'
+    },
+    {
+        'author'     : 'goody',
+        'title'      : 'blog post 2',
+        'content'    : 'second post content',
+        'date_posted': 'march 18 2021'
+    }
+]
+
+def home(request):
+    context = {
+        'posts' : posts
+    }
+    return render(request, 'blog/home.html', context)
+
+ 
+def about(request):
+    return render(request, 'blog/about.html')
